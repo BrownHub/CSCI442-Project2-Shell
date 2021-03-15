d7aebe81f1f0c076121a9f7b08797cb28988f608
Kendall Brown <kendallcraigbrown@mymail.mines.edu>
2021-03-14 20:25:15 -0600

    Added correct dispatcher.c file

--- src/dispatcher.c
+++ src/dispatcher.c
@@ -5,6 +5,8 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
 #include "dispatcher.h"
 #include "shell_builtins.h"
@@ -47,28 +49,186 @@ static int dispatch_external_command(struct command *pipeline)
 	 * may live with the assumption that the "input_file" field in
 	 * the pipeline struct you are given is NULL, and that
 	 * "output_type" will always be COMMAND_OUTPUT_STDOUT.
-	 *
+	 * 
 	 * For D2: you'll extend this function to support input and
 	 * output files, as well as pipeline functionality.
 	 *
 	 * Good luck!
 	 */
 
-	struct command *pipe = pipeline;
-	pid_t pid;
+	/*
+	If input != null
+		pipe input file into first command
+	while output is pipe
+		execute command
+		pipe command to next command
+	if output is redirect(truncate)
+		if file does not exist
+			make it
+		redirect to overwrite file
+	if output is redirect(append)
+		if file does not exist
+			make it
+		redirect to append to file
+	*/
 
+	//Initialize variables
+	struct command *the_pipe = pipeline;
+	pid_t pid;
+	int child_status;
+
+	//Variables for pipeline
+	int pipe_fd[2];
+	pipe(pipe_fd);
+	int fd_read = pipe_fd[0];
+	int fd_write = pipe_fd[1];
+	int fd_read_next;
+
+	//Code for pipe commands
+	if (the_pipe->output_type == COMMAND_OUTPUT_PIPE){
+		if((pid = fork()) < 0){
+			fprintf(stderr, "fork error\n");
+		}
+		else if (pid == 0){
+			//Redirects input
+			if (the_pipe->input_filename != NULL){
+				int input_fd = open(the_pipe->input_filename,
+					       	O_RDONLY, 0666);
+				dup2(input_fd, STDIN_FILENO);
+			}
+
+			close(fd_read);
+			dup2(fd_write, STDOUT_FILENO);
+
+			if(execvp(the_pipe->argv[0], the_pipe->argv)){
+				perror("Failed to execute");
+				fprintf(stderr, "exec error\n");
+			}
+			exit(-1);
+		}
+
+		close(fd_write);
+
+		if(waitpid(pid, &child_status, 0) != pid){
+			fprintf(stderr, "waitpid error\n");
+		}
+
+		if(child_status != 0){
+			return -1;
+		}
+
+		the_pipe = the_pipe->pipe_to;
+	
+
+		while (the_pipe->output_type == COMMAND_OUTPUT_PIPE){
+	
+			pipe(pipe_fd);
+			fd_write = pipe_fd[1];
+			fd_read_next = pipe_fd[0];	
+
+			if((pid = fork()) < 0){
+				fprintf(stderr, "fork error\n");
+			}
+			else if (pid == 0){
+				dup2(fd_read, STDIN_FILENO);
+				dup2(fd_write, STDOUT_FILENO);
+	
+				if(execvp(the_pipe->argv[0], the_pipe->argv)){
+					perror("Failed to execute");
+					fprintf(stderr, "exec error\n");
+				}
+				exit(-1);
+			}
+
+			if(waitpid(pid, &child_status, 0) != pid){
+				fprintf(stderr, "waitpid error\n");
+			}
+
+			close(fd_write);
+			close(fd_read);
+
+			if(child_status != 0){
+				return -1;
+			}
+
+			fd_read = fd_read_next;
+		
+			the_pipe = the_pipe->pipe_to;	
+
+		}
+
+		if((pid = fork()) < 0){
+			fprintf(stderr, "fork error\n");
+		}
+		else if (pid == 0){
+			//Redirects output
+			if (the_pipe->output_type == COMMAND_OUTPUT_FILE_APPEND){
+			int output_fd = open(the_pipe->output_filename,
+				       	O_WRONLY | O_CREAT | O_APPEND , 0666);
+			dup2(output_fd, STDOUT_FILENO);
+			}
+			else if (the_pipe->output_type == COMMAND_OUTPUT_FILE_TRUNCATE){
+			int output_fd = open(the_pipe->output_filename,
+				       	O_WRONLY | O_CREAT | O_TRUNC , 0666);
+			dup2(output_fd, STDOUT_FILENO);
+			}
+
+			dup2(fd_read, STDIN_FILENO);
+
+			if(execvp(the_pipe->argv[0], the_pipe->argv)){
+				perror("Failed to execute");
+				fprintf(stderr, "exec error\n");
+			}
+			exit(-1);
+		}
+
+		close(fd_read);
+
+		if(waitpid(pid, &child_status, 0) != pid){
+			fprintf(stderr, "waitpid error\n");
+		}
+
+		if(child_status != 0){
+			return -1;
+		}
+
+		return 0;
+	
+	}
+
+	//Code for non-piped command execution
 	if((pid = fork()) < 0){
 		fprintf(stderr, "fork error\n");
 	}
 	else if (pid == 0){
-		if(execvp(pipe->argv[0], pipe->argv)){
+		//Redirects input
+		if (the_pipe->input_filename != NULL){
+			int input_fd = open(the_pipe->input_filename,
+				       	O_RDONLY, 0666);
+			dup2(input_fd, STDIN_FILENO);
+		}
+
+		//Redirects output
+		if (the_pipe->output_type == COMMAND_OUTPUT_FILE_APPEND){
+		int output_fd = open(the_pipe->output_filename,
+			       	O_WRONLY | O_CREAT | O_APPEND , 0666);
+		dup2(output_fd, STDOUT_FILENO);
+		}
+		else if (the_pipe->output_type == COMMAND_OUTPUT_FILE_TRUNCATE){
+		int output_fd = open(the_pipe->output_filename,
+			       	O_WRONLY | O_CREAT | O_TRUNC , 0666);
+		dup2(output_fd, STDOUT_FILENO);
+		}
+
+		if(execvp(the_pipe->argv[0], the_pipe->argv)){
 			perror("Failed to execute");
-			fprintf(stderr, "exec error\n");
+				
 		}
 		exit(-1);
 	}
 
-	int child_status;
+	close(fd_read);
+	close(fd_write);
 
 	if(waitpid(pid, &child_status, 0) != pid){
 		fprintf(stderr, "waitpid error\n");
